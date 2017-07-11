5737738f30b0549bf83b7a884c13b5e4cc86cf10
Mike A. Trethewey <miketrethewey@gmail.com>
2017-07-11 12:37:41 -0700

    Make flow control explicit

--- script/tracker.js
+++ script/tracker.js
@@ -1,6 +1,6 @@
-var rowLength	= 7;
-var prizes		= [];
-var medallions	= [0, 0];
+var rowLength   = 7;
+var prizes      = [];
+var medallions  = [0, 0];
 
 function $(e) {
   return document.getElementById(e);
@@ -9,9 +9,10 @@ function $(e) {
 // Event of clicking on the item tracker
 function toggle(label) {
   if(label.substring(0,5) == "chest") {
-    if(--items[label] < 0)
+    if(--items[label] < 0) {
       items[label] = itemsMax[label];
-      $(label).style.backgroundImage = ("url(images/chest" + items[label] + ".png)");
+    }
+    $(label).style.backgroundImage = ("url(images/chest" + items[label] + ".png)");
     x = label.substring(5);
 //    if(items[label] == 0)
 //      $("dungeon" + x).className = "dungeon opened";
@@ -22,32 +23,35 @@ function toggle(label) {
 
   if((typeof items[label]) == "boolean") {
     $(label).className = (items[label] = !items[label]);
-  }
-  else {
-    if(++items[label] > itemsMax[label]){
+  } else {
+    if(++items[label] > itemsMax[label]) {
       items[label] = itemsMin[label];
-        $(label).style.backgroundImage = ("url(images/" + label + ".png)");
-      if(!items[label])
-      $(label).className = ("false");
-    }
-    else{
-        $(label).style.backgroundImage = ("url(images/" + label + items[label] + ".png)");
+      $(label).style.backgroundImage = ("url(images/" + label + ".png)");
+      if(!items[label]) {
+        $(label).className = ("false");
+      }
+    } else {
+      $(label).style.backgroundImage = ("url(images/" + label + items[label] + ".png)");
       $(label).className = ("true");
     }
   }
 
   // Initiate bunny graphics!
-    if(label == "moonpearl" || label == "tunic")
-      togglePearl();
+  if(label == "moonpearl" || label == "tunic") {
+    togglePearl();
+  }
 }
 
 // BUNNY TIME!!!
 function togglePearl() {
   var link = "url(images/tunic";
-  if(items.tunic > 1)
+  if(items.tunic > 1) {
     link += items.tunic;
-  if(!items.moonpearl)
+  }
+  if(!items.moonpearl) {
     link += "b";
+  }
+
   link += ".png)";
 
   $("tunic").style.backgroundImage = link;
@@ -56,16 +60,18 @@ function togglePearl() {
 // event of clicking on a boss's pendant/crystal subsquare
 function toggleDungeon(n) {
   prizes[n]++;
-  if(prizes[n] == 5)
+  if(prizes[n] == 5) {
     prizes[n] = 0;
+  }
   $("dungeonPrize" + n).style.backgroundImage = "url(images/dungeon" + prizes[n] + ".png)";
 }
 
 // event of clicking on Mire/TRock's medallion subsquare
 function toggleMedallion(n) {
   medallions[n]++;
-  if(medallions[n] == 4)
+  if(medallions[n] == 4) {
     medallions[n] = 0;
+  }
   $("medallion" + n).style.backgroundImage = "url(images/medallion" + medallions[n] + ".png)";
 }
 
@@ -102,18 +108,20 @@ function print_lonk() {
 function print_tracker() {
   print_lonk();
 
-  var itemList	= Object.keys(items);
-  var k			= 4;
-  while(k < itemList.length){
-    if(k != 4)
+  var itemList  = Object.keys(items);
+  var k     = 4;
+  while(k < itemList.length) {
+    if(k != 4) {
       document.write("<tr>");
-    for(j = 0; j < rowLength; j++){
-      if(k == 4 || k == 9)
+    }
+    for(j = 0; j < rowLength; j++) {
+      if(k == 4 || k == 9) {
         j = 2;
+      }
       var x = itemList[k];
 
         // boss tables (pendant/crystal, medallion)
-        if(x.substring(0,4) == "boss"){
+        if(x.substring(0,4) == "boss") {
           var d = prizes.length;
           prizes[d] = 0;
 
@@ -125,29 +133,30 @@ function print_tracker() {
             document.write("</tr>");
             document.write("<tr>");
               // Does dungeon have a medallion?
-              if(d >= 8)
+              if(d >= 8) {
                 document.write("<th id='medallion" + (d - 8) + "' onClick='toggleMedallion(" + (d - 8) + ")' class='corner' style='background-image:url(images/medallion0.png)'></th>");
-              else
+              } else {
                 document.write("<th onClick='toggle(" + '"' + x + '"' + ")'></th>");
+              }
               document.write("<th id='dungeonPrize" + d + "' onClick='toggleDungeon(" + d + ")' class='corner' style='background-image:url(images/dungeon0.png)'></th>");
             document.write("</tr>");
             document.write("</table>");
           document.write("</td>");
-        }
-        else {
-          if(x.substring(0,5) == "chest"){
+        } else {
+          if(x.substring(0,5) == "chest") {
             document.write("<td style='background-image:url(images/chest" + items[x] + ".png);' ");
-          }
-          else
+          } else {
             document.write("<td style='background-image:url(images/" + x + ".png);' ");
+          }
           document.write("id='" + x + "' ");
           document.write("class='" + !!items[x] + "' ");
           document.write("onClick='toggle(" + '"' + x + '"' + ")'>");
           document.write("</td>");
         }
 
-      if(++k == itemList.length)
-      break;
+      if(++k == itemList.length) {
+        break;
+      }
     }
     document.write("</tr>");
   }
@@ -155,6 +164,6 @@ function print_tracker() {
 }
 
 function init() {
-  $("sword").style.backgroundImage	= "url(images/sword1.png)";
-  $("shield").style.backgroundImage	= "url(images/shield.png)";
+  $("sword").style.backgroundImage  = "url(images/sword1.png)";
+  $("shield").style.backgroundImage = "url(images/shield.png)";
 }
