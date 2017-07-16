7e4152d4396eb23198ca748fe842b41590f53e8b
Mike A. Trethewey <miketrethewey@gmail.com>
2017-07-16 01:22:57 -0700

    Divorce item stats from each other
    Preparing for individual profiles instead of just one array of items

---
+++ script/items-max.js
@@ -0,0 +1,35 @@
+var itemsMax = {
+    sword:4,
+    shield:3,
+    tunic:3,
+
+    bottle:4,
+    bow:3,
+    boomerang:3,
+    glove:2,
+
+    boss0: 2,
+    boss1: 2,
+    boss2: 2,
+
+    agahnim:1,
+
+    boss3: 2,
+    boss4: 2,
+    boss5: 2,
+    boss6: 2,
+    boss7: 2,
+    boss8: 2,
+    boss9: 2,
+
+    chest0: 3,
+    chest1: 2,
+    chest2: 2,
+    chest3: 5,
+    chest4: 6,
+    chest5: 2,
+    chest6: 4,
+    chest7: 3,
+    chest8: 2,
+    chest9: 5
+};
