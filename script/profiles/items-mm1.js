6bd7ff5e8edd710a0d88d4b4f1ca6eff420e1bce
Mike A. Trethewey <miketrethewey@gmail.com>
2017-07-27 22:41:36 -0700

    Re-factor JS files

---
+++ script/profiles/items-mm1.js
@@ -0,0 +1,99 @@
+var items = {
+    tunic:      1,
+    sword:      1,
+    shield:     0,
+    moonpearl:  false,
+
+    chest5:     2,
+    chest1:     2,
+    chest2:     2,
+    chest0:     3,
+    chest9:     5,
+    chest6:     4,
+    chest8:     2,
+    chest4:     6,
+    chest7:     3,
+    chest3:     5,
+
+    mushroom:   false,
+    powder:     false,
+    boss5:      1, // SkWoods
+    boss1:      1, // Desert
+    boss2:      1, // Hera
+    boss0:      1, // Eastern
+    boss9:      1, // TRock
+
+    shovel:     false,
+    flute:      false,
+    boss6:      1, // Thieves'
+    boss8:      1, // Mire
+    boss4:      1, // Swamp
+    boss7:      1, // Ice
+    boss3:      1, // PoD
+
+    boots:    false,
+    glove:    0,
+    flippers: false,
+    firerod:  false,
+    icerod:   false,
+    somaria:  false,
+    byrna:    false,
+
+    bombos:       false,
+    ether:        false,
+    quake:        false,
+    pendant0:     false,
+    pendant1:     false,
+    pendant2:     false,
+    chestgt:      22,
+
+    blumerang:    false,
+    redmerang:    false,
+
+    bottleItemA:  0,
+    bottleItemB:  0,
+    bottleItemC:  0,
+    bottleItemD:  0,
+    agahnim:  0,
+
+    bow:          0,
+    "duoMerang-blumerang-redmerang":    0,
+    hookshot:     false,
+    bomb:         false,
+    "quadFetch-mushroom-powder-shovel-flute": false,
+    "quadDungeonMedallions-label9-medallionT-label8-medallionM":   0,
+    mpupgrade:    0,
+
+    "quadSticks-firerod-icerod-somaria-byrna": false,
+    "triMedallions-bombos-ether-quake": false,
+    nothing8: 0,
+    "triEquipment-tunic-shield-sword": 0,
+    "quadAItems-boots-glove-flippers-moonpearl": false,
+    "triPendants-pendant0-pendant1-pendant2": 0,
+    "countCrystals":  0,
+
+    lantern:    false,
+    hammer:     false,
+    net:        false,
+    book:       false,
+    "quadBottleContents-bottleItemA-bottleItemB-bottleItemC-bottleItemD": 0,
+    nothing12:  0,
+    nothing13:  0,
+
+    bottle:     0,
+    cape:       false,
+    mirror:     false,
+    "quadLWBosses-boss0-boss1-boss2-agahnim": 0,
+    "quadDWBosses1-boss3-boss4-boss5-boss6": 0,
+    "triDWBosses2-boss7-boss8-boss9": 0
+};
+
+itemsMin["bottleItemA"] = 0;
+itemsMin["bottleItemB"] = 0;
+itemsMin["bottleItemC"] = 0;
+itemsMin["bottleItemD"] = 0;
+
+itemsMax["bottleItemA"] = 6;
+itemsMax["bottleItemB"] = 6;
+itemsMax["bottleItemC"] = 6;
+itemsMax["bottleItemD"] = 6;
